af67a8a3f1b87e1b0bebeca004871dfa9d8dfabb
Annie Lee <annie80113@hotmail.com>
2021-09-10 20:36:04 -0400

    updated style for toDo page

--- idkwiw-react-app/src/components/TodoDetail/TodoDetail.js
+++ idkwiw-react-app/src/components/TodoDetail/TodoDetail.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function ToDoDetail({toDo}) {
   return (
-    <div className='showBox'>
+    <div className='toDoShowBox'>
       {/* As a player, I want to see a placeholder activity name on to do page */}
       <h2>{toDo.activity}</h2>
     </div>
