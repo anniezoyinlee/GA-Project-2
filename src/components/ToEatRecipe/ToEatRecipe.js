9b1e74cc06f9dd85a13ec6bbd6b7bfd0f2d27d56
Annie Lee <annie80113@hotmail.com>
2021-09-14 21:32:51 -0400

    added comments

--- src/components/ToEatRecipe/ToEatRecipe.js
+++ src/components/ToEatRecipe/ToEatRecipe.js
@@ -12,6 +12,8 @@ function ToEatRecipe({toEat}) {
         url={toEat.strYoutube} 
         width='100%' 
         height='100%' 
+        // For fixing Failed to execute 'postMessage' on 'DOMWindow' issue
+        // reference: https://github.com/CookPete/react-player/issues/508
         config={{ youtube: { playerVars: { origin: 'https://www.youtube.com' } } }}
         />
       </div>
